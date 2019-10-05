66d2be3a1dd5e2019c4dd0eb3c15e00963468939
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2019-10-05 05:17:54 -0300

    feito configurações do SpringSegurety

--- src/main/java/br/com/codenation/centralerros/segurity/AuthorizationServerConfig.java
+++ src/main/java/br/com/codenation/centralerros/segurity/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -31,7 +32,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
         clients.inMemory()
                 .withClient("client-id")
-                .secret("secret-id")
+                .secret(new BCryptPasswordEncoder().encode("secret-id"))
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
                 .scopes("read", "write", "trust");
 
--- src/main/java/br/com/codenation/centralerros/segurity/WebSecurityConfigAdapter.java
+++ src/main/java/br/com/codenation/centralerros/segurity/WebSecurityConfigAdapter.java
@@ -1,5 +1,8 @@
 package br.com.codenation.centralerros.segurity;
 
+import br.com.codenation.centralerros.dto.UsuarioCustomDTO;
+import br.com.codenation.centralerros.entity.Usuario;
+import br.com.codenation.centralerros.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -14,16 +20,29 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
 
     @Bean
     public AuthenticationManager customAuthenticationManager() throws Exception {
-
         return authenticationManagerBean();
-
     }
 
     @Autowired
-    public void authenticationManager(AuthenticationManagerBuilder builder, UsuarioR) throws Exception {
+    public void authenticationManager(AuthenticationManagerBuilder builder, UsuarioRepository usuarioRepository) throws Exception {
 
+        if (usuarioRepository.count() == 0 ) {
+            Usuario usuario = new Usuario();
+            usuario.setEmail("teste@gmail.com");
+            usuario.setSenha(passwordEncoder().encode("123"));
+            usuario.setNome("teste");
+            usuario.setToken(UUID.randomUUID().toString());
+            usuarioRepository.saveAndFlush(usuario);
+        }
 
+        builder.userDetailsService(login -> new UsuarioCustomDTO(usuarioRepository.findByEmail(login)))
+                .passwordEncoder(passwordEncoder());
 
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
